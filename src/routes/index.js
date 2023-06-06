57613b83f7d24ccdd79ccdb2778ea1017d7f8daf
Hamed Osama <hamedosama19@gmail.com>
2023-06-06 23:41:46 +0300

    FIX Issue

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,8 +2,6 @@ const { Router } = require('express');
 
 const userRoutes = require('./api/user.routes');
 const productRoutes = require('./api/product.routes');
-const orderRoutes = require('./api/order.routes');
-const maintenanceRoutes = require('./api/maintenance.routes');
 const adminRoutes = require('./api/admin.routes');
 const imageRoutes = require('./api/image.routes');
 
@@ -11,9 +9,7 @@ const routes = Router();
 
 routes.use('/users', userRoutes);
 routes.use('/products', productRoutes);
-// routes.use('/orders', orderRoutes);
 routes.use('/admin', adminRoutes);
-// routes.use('/maintenances', maintenanceRoutes);
 // routes.use('/images', imageRoutes);
 
 module.exports = routes;
